4ec31a0792c2381c3831b8f2d50c869b782d6160
gl8429 <lucifer92.lin@gmail.com>
2016-02-15 16:25:50 -0600

    create report

---
+++ Project1/report/WofTest.R
@@ -0,0 +1,16 @@
+ATIS <- c(24.054, 27.161, 12.700)
+WSJ <- c(275.118, 266.352, 126.113)
+BROWN <- c(310.667, 299.686, 167.487)
+g_range <- range(0, ATIS, WSJ, BROWN)
+plot_colors <- c("blue", "red", "forestgreen")
+plot(ATIS, type = "o", col = plot_colors[1], ylim = g_range, axes = FALSE, ann = FALSE)
+axis(1, at = 1:3, lab = c("Forward", "Backward", "Bidirectional"))
+axis(2, las=1, at=50*0:g_range[2])
+box()
+lines(WSJ, type="o", pch=22, lty=2, col=plot_colors[2])
+lines(BROWN, type="o", pch=22, lty=2, col=plot_colors[3])
+title(main="Word Preplexity of Testing Data", col.main="red", font.main=4)
+title(xlab= "Bigram Model", col.lab=rgb(0,0.5,0))
+title(ylab= "Word Preplexity", col.lab=rgb(0,0.5,0))
+legend(1, g_range[2]*0.5, c("ATIS", "WSJ", "BROWN"), cex=0.8, col=plot_colors, pch=21:23, lty=1:3);
+
